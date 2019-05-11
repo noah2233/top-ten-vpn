e5126e004b58eced318580f524256b5371a4d424
Noah Lubko <nlubko@gmail.com>
2019-05-11 15:35:54 +0300

    add pages

--- src/app/core/consts/pages.ts
+++ src/app/core/consts/pages.ts
@@ -22,8 +22,49 @@ export const bestVpnFor = {
         uk: { href: 'uk', name: 'UK' },
         australia: { href: 'australia', name: 'Australia' },
         france: { href: 'france', name: 'France' }
+    },
+    popularServices: {
+        name: 'Popular Services',
+        netflix: { href: 'netflix', name: 'Netflix' },
+        kodi: { href: 'kodi', name: 'Kodi' },
+        amazonPrime: { href: 'amazonPrime', name: 'Amazon Prime' },
+        hulu: { href: 'hulu', name: 'Hulu' },
+        bbcIplayer: { href: 'bbcIplayer', name: 'bbcIPlayer' },
+        fobuTV: { href: 'fobuTV', name: 'Fobu TV' },
+        directTVNow: { href: 'directtv-now', name: 'DirectTV Now' }
+    },
+    popularUsage: {
+        name: 'Popular Usage',
+        onlineGaming: { href: 'online-gaming', name: 'Online Gaming' },
+        streaming: { href: 'streaming', name: 'Streaming' },
+        torrenting: { href: 'torrenting', name: 'Torrenting' },
+        sports: { href: 'sports', name: 'Sports' },
+        facebook: { href: 'facebook', name: 'Facebook' },
+        twitter: { href: 'twitter', name: 'Twitter' },
+        youtube: { href: 'youtube', name: 'Youtube' },
+        telegram: { href: 'telegram', name: 'Telegram' },
+        businesses: { href: 'businesses', name: 'Businesses' },
     }
 };
-export const vpnReviews = { href: 'vpn-reviews', name: 'VPN Reviews' };
-export const guides = { name: 'Guides' };
+export const vpnReviews = {
+    href: 'vpn-reviews', name: 'VPN Reviews',
+    reviews: {
+        cyberGhost: { href: 'cyber-ghost', name: 'Cyber Ghost' },
+        nordVPN: { href: 'nordvpn', name: 'NordVPN' },
+        privateVPN: { href: 'privatevpn', name: 'privateVPN' },
+        surfShark: { href: 'surfshark', name: 'SurfShark' },
+        zenMate: { href: 'zenmate', name: 'ZenMate' }
+    }
+};
+export const guides = {
+    name: 'Guides',
+    guides: {
+        howToWatchFifaWorldCup2018: { href: 'how-to-watch-fifa-world-cup-2018', name: 'How to Watch FiFA World Cup 2018' },
+        howToChooseAVPN: { href: 'how-to-choose-a-vpn', name: 'How to Choose a VPN' },
+        howToSurfTheWebAnonymously: { href: 'how-to-surf-The-Web-Anonymously', name: 'How to Surf the Web Anonymously' },
+        cheapestVPNs: { href: 'cheapest-vpns', name: 'Cheapest VPNs' },
+        howToBypassVPNBlocks: { href: 'how-to-Bypass-vpn-blocks', name: 'How to Bypass VPN Blocks' },
+        guideToGeoSpoofing: { href: 'guide-To-Geo-Spoofing', name: 'Guide to Geo Spoofing' }
+    }
+};
 export const learn = { href: 'learn', name: 'Learn' };
