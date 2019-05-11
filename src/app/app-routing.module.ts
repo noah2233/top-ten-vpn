8098a6a761c4f3cd071541f49f7d68c270389608
Noah Lubko <nlubko@gmail.com>
2019-05-11 09:47:02 +0300

    change to href

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   {
     path: pages.bestVpnFor.href, component: BestVpnForComponent,
     children: [
-      { path: pages.bestVpnFor.devicesOrOs.windows.name, component: WindowsComponent },
+      { path: pages.bestVpnFor.devicesOrOs.windows.href, component: WindowsComponent },
     ]
   },
   { path: '**', component: TopTenVpnComponent }
