95a05f4f7a3a715ca1d676b3018ae5d0a45b18ac
Noah Lubko <nlubko@gmail.com>
2019-06-10 16:03:52 +0300

    add resources

---
+++ src/app/pages/top-ten-vpn/resources/topTenVpns.ts
@@ -0,0 +1,20 @@
+import { TopTenVpnService } from '@core/interface';
+
+const topTenVpns: TopTenVpnService[] = [
+    {
+        name: 'cyberGhost',
+        mainFeatures: {
+            title: 'High-speed streaming, browsing, and gaming with complete security',
+            mainFeaturesList: [
+                'Access Netflix wherever you are',
+                '3,700+ servers worldwide',
+                '7 simultaneous connections',
+                'Friendly chat and email support',
+                '45-day money-back guarantee'
+            ]
+        },
+        score: null,
+        link: 'https://www.cyberghostvpn.com'
+    }
+];
+
