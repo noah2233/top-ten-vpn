60294b31e29af106f057dfca3c98f856fa2c52dc
Noah Lubko <nlubko@gmail.com>
2019-06-15 16:56:33 +0300

    add initBestVPNForItems

--- src/app/pages/best-vpn-for/best-vpn-for-side-menu/best-vpn-for-side-menu.component.ts
+++ src/app/pages/best-vpn-for/best-vpn-for-side-menu/best-vpn-for-side-menu.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+import { forEach } from 'lodash';
+
+import * as templates from '@pages/best-vpn-for/templates/templates';
 @Component({
   selector: 'best-vpn-for-side-menu',
   templateUrl: './best-vpn-for-side-menu.component.html',
   styleUrls: ['./best-vpn-for-side-menu.component.css']
 })
 export class BestVpnForSideMenuComponent implements OnInit {
+  @Input() templateName;
+  bestVPNForItems = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.initBestVPNForItems();
   }
 
+  initBestVPNForItems() {
+    const templatesAarrayNames = [
+      'android',
+      'china',
+      'onlineGaming',
+      'kodi',
+      'iphone',
+      'chrome',
+    ];
+
+    const that = this;
+    forEach(templatesAarrayNames, function (name) {
+      if (name !== that.templateName) {
+        that.bestVPNForItems.push(templates[name]);
+      }
+    });
+  }
 }
