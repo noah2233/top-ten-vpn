192f692b33b58c928b1007dbdb328a5624808cca
Noah Lubko <nlubko@gmail.com>
2019-06-15 15:25:46 +0300

    add templates

--- src/app/pages/best-vpn-for/templates/templates.ts
+++ src/app/pages/best-vpn-for/templates/templates.ts
@@ -1,6 +1,38 @@
 declare var require: any;
 const windowsHtml = require('./windows.html');
 const macHtml = require('./mac.html');
+const chromeHtml = require('./chrome.html');
+const firefoxHtml = require('./firefox.html');
+const androidHtml = require('./android.html');
+const iphoneHtml = require('./iphone.html');
+const ipadOrTabletHtml = require('./ipadOrTablet.html');
+const appletvHtml = require('./appletv.html');
+const smarttvHtml = require('./smarttv.html');
+const routerHtml = require('./router.html');
+
+const chinaHtml = require('./china.html');
+const russiaHtml = require('./russia.html');
+const ukHtml = require('./uk.html');
+const australiaHtml = require('./australia.html');
+const franceHtml = require('./france.html');
+
+const netflixHtml = require('./netflix.html');
+const kodiHtml = require('./kodi.html');
+const amazonPrimeHtml = require('./amazonPrime.html');
+const huluHtml = require('./hulu.html');
+const bbcIPlayerHtml = require('./bbcIPlayer.html');
+const fubotvHtml = require('./fubotv.html');
+const directTvNowHtml = require('./directTvNow.html');
+
+const onlineGamingHtml = require('./onlineGaming.html');
+const streamingHtml = require('./streaming.html');
+const torrentingHtml = require('./torrenting.html');
+const sportsHtml = require('./sports.html');
+const facebookHtml = require('./facebook.html');
+const twitterHtml = require('./twitter.html');
+const youtubeHtml = require('./youtube.html');
+const telegramHtml = require('./telegram.html');
+const businessesHtml = require('./businesses.html');
 
 export const windows = {
     title: 'The Best VPNs for Windows of 2019',
