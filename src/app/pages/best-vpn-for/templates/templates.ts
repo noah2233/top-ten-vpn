c4b3183f6eb1c82ac1af7b023dbe161250f7ba2a
Noah Lubko <nlubko@gmail.com>
2019-06-15 15:38:53 +0300

    add template

--- src/app/pages/best-vpn-for/templates/templates.ts
+++ src/app/pages/best-vpn-for/templates/templates.ts
@@ -45,3 +45,177 @@ export const mac = {
     date: 'July 1, 2019',
     main: macHtml
 };
+
+export const chrome = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: chromeHtml
+};
+
+export const firefox = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: firefoxHtml
+};
+
+export const android = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: androidHtml
+};
+
+export const iphone = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: iphoneHtml
+};
+
+export const ipadOrTablet = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: ipadOrTabletHtml
+};
+
+export const appletv = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: appletvHtml
+};
+
+export const smarttv = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: smarttvHtml
+};
+
+export const router = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: routerHtml
+};
+
+export const china = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: chinaHtml
+};
+
+export const russia = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: russiaHtml
+};
+
+export const uk = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: ukHtml
+};
+
+export const australia = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: australiaHtml
+};
+
+export const france = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: franceHtml
+};
+
+export const netflix = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: netflixHtml
+};
+
+export const kodi = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: kodiHtml
+};
+
+export const amazonPrime = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: amazonPrimeHtml
+};
+
+export const hulu = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: huluHtml
+};
+
+export const bbcIPlayer = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: bbcIPlayerHtml
+};
+
+export const fubotv = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: fubotvHtml
+};
+
+export const directTvNow = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: directTvNowHtml
+};
+
+export const onlineGaming = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: onlineGamingHtml
+};
+
+export const streaming = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: streamingHtml
+};
+
+export const torrenting = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: torrentingHtml
+};
+
+export const sports = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: sportsHtml
+};
+
+export const facebook = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: facebookHtml
+};
+
+export const twitter = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: twitterHtml
+};
+
+export const youtube = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: youtubeHtml
+};
+
+export const telegram = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: telegramHtml
+};
+
+export const businesses = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: businessesHtml
+};
