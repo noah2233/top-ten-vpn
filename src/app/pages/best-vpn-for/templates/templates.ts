c107e976754acdc318b9f5d9f33d5fd870e7c338
Noah Lubko <nlubko@gmail.com>
2019-06-15 14:21:22 +0300

    add templates

--- src/app/pages/best-vpn-for/templates/templates.ts
+++ src/app/pages/best-vpn-for/templates/templates.ts
@@ -1,8 +1,15 @@
 declare var require: any;
 const windowsHtml = require('./windows.html');
+const macHtml = require('./mac.html');
 
 export const windows = {
     title: 'The Best VPNs for Windows of 2019',
     date: 'July 1, 2019',
     main: windowsHtml
+};
+
+export const mac = {
+    title: 'The Best VPNs for Windows of 2019',
+    date: 'July 1, 2019',
+    main: macHtml
 };
