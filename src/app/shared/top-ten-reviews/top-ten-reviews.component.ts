6919ffe52c6e57ad36417fb3c6ad21b4be320c25
Noah Lubko <nlubko@gmail.com>
2019-07-03 09:52:13 +0300

    add initTopTenReviews

--- src/app/shared/top-ten-reviews/top-ten-reviews.component.ts
+++ src/app/shared/top-ten-reviews/top-ten-reviews.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Reviews } from '@DB/reviewsDB';
+
+import { Review } from '@core/interface';
+
+import { orderBy } from 'lodash';
 @Component({
   selector: 'top-ten-reviews',
   templateUrl: './top-ten-reviews.component.html',
   styleUrls: ['./top-ten-reviews.component.css']
 })
 export class TopTenReviewsComponent implements OnInit {
+  reviews: Review[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.initTopTenReviews();
   }
 
+  initTopTenReviews() {
+    // const _reviews = Reviews;
+    this.reviews = orderBy(Reviews, ['score'], ['asc']).slice(0, 5);
+  }
 }
