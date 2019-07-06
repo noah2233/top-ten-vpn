547b36bb60ba4fa19a2209967b8996d08d49f35e
Noah Lubko <nlubko@gmail.com>
2019-07-06 11:29:18 +0300

    change names

--- src/app/DB/pages.ts
+++ src/app/DB/pages.ts
@@ -8,24 +8,19 @@ export const bestVpnFor: Page = {
             name: 'Devices/OS',
             pages: [
                 {
-                    name: 'Windows',
-                    path: 'best-vpns-for-windows'
+                    name: 'Windows', path: 'best-vpns-for-windows'
                 },
                 {
-                    name: 'Mac',
-                    path: 'best-vpns-for-mac'
+                    name: 'Mac', path: 'best-vpns-for-mac'
                 },
                 {
-                    name: 'Chrome',
-                    path: 'best-vpns-for-google-chrome'
+                    name: 'Chrome', path: 'best-vpns-for-google-chrome'
                 },
                 {
-                    name: 'Firefox',
-                    path: 'best-vpns-for-firefox'
+                    name: 'Firefox', path: 'best-vpns-for-firefox'
                 },
                 {
-                    name: 'Android',
-                    path: 'best-vpns-for-android'
+                    name: 'Android', path: 'best-vpns-for-android'
                 },
                 {
                     name: 'IPhone',
@@ -157,23 +152,23 @@ export const vpnReviews: Page = {
         {
             name: null,
             pages: [
-                { name: 'CyberGhost', path: 'cyberghost', score: 9.9, offer: 'https://www.cyberghostvpn.com' },
+                { name: 'Cyber Ghost', path: 'cyberghost', score: 9.9, offer: 'https://www.cyberghostvpn.com' },
                 { name: 'NordVPN', path: 'nordvpn', score: 9.5, offer: 'https://www.nordvpn.com' },
                 { name: 'PrivateVPN', path: 'privatevpn', score: 9.1, offer: 'https://www.privatevpn.com' },
                 { name: 'ZenMate', path: 'zenmate', score: 9.0, offer: 'https://www.zenmate.com' },
                 { name: 'SurfShark', path: 'surfshark', score: 8.8, offer: 'https://www.surfshark.com' },
-                { name: 'PureVPN', path: 'purevpn', score: 8.5, offer: 'https://www.purevpn.com' },
+                { name: 'Pure VPN', path: 'purevpn', score: 8.5, offer: 'https://www.purevpn.com' },
                 { name: 'BullGuard', path: 'bullguard', score: 8.3, offer: 'https://www.bullguard.com' },
                 { name: 'Panda', path: 'panda', score: 8.2, offer: 'https://www.pandasecurity.com' },
                 { name: 'HideMyAss', path: 'hidemyass', score: 8.0, offer: 'https://www.hidemyass.com' },
                 { name: 'SaferVPN', path: 'safervpn', score: 7.9, offer: 'https://www.safervpn.com' },
                 { name: 'UltraVPN', path: 'ultravpn', score: 8.6, offer: 'https://www.ultravpn.com' },
                 { name: 'TunnelBear', path: 'tunnelbear', score: 8.0, offer: 'https://www.tunnelbear.com' },
-                { name: 'Perimeter81', path: 'perimeter81', score: 7.0, offer: 'https://www.perimeter81.com' },
-                { name: 'AviraPhantomVPN', path: 'aviraphantomvpn', score: 6.9, offer: 'https://www.avira.com' },
-                { name: 'HotspotShieldElite', path: 'hotspotshieldelite', score: 6.7, offer: 'https://www.gethotspotshield.com' },
+                { name: 'Perimeter 81', path: 'perimeter81', score: 7.0, offer: 'https://www.perimeter81.com' },
+                { name: 'Avira Phantom VPN', path: 'aviraphantomvpn', score: 6.9, offer: 'https://www.avira.com' },
+                { name: 'Hotspot Shield Elite', path: 'hotspotshieldelite', score: 6.7, offer: 'https://www.gethotspotshield.com' },
                 { name: 'ExpressVPN', path: 'expressvpn', score: 6.6, offer: 'https://www.expressvpn.com' },
-                { name: 'McAfeeSafeConnect', path: 'mcafeesafeconnect', score: 6.4, offer: 'https://www.mcafee.hsselite.com' },
+                { name: 'McAfee Safe Connect', path: 'mcafeesafeconnect', score: 6.4, offer: 'https://www.mcafee.hsselite.com' },
                 { name: 'SpyOFF', path: 'spyoff', score: 6.4, offer: 'https://www.shop.spyoff.com' },
                 { name: 'AvastVPN', path: 'avastvpn', score: 6.3, offer: 'https://www.avast.com' },
                 {
@@ -185,18 +180,18 @@ export const vpnReviews: Page = {
                 { name: 'SurfEasy', path: 'surfeasy', score: 5.9, offer: 'https://www.surfeasy.com' },
                 { name: 'HideMe', path: 'hideme', score: 5.7, offer: 'https://www.hide.me' },
                 { name: 'IPVanish', path: 'ipvanish', score: 5.7, offer: 'https://www.ipvanish.me' },
-                { name: 'StrongVPN', path: 'strongvpn', score: 5.6, offer: 'https://www.strongvpn.com' },
+                { name: 'Strong VPN', path: 'strongvpn', score: 5.6, offer: 'https://www.strongvpn.com' },
                 { name: 'OverPlay', path: 'overplay', score: 5.5, offer: 'https://www.overplay.net' },
                 {
-                    name: 'PrivateInternetAccess', path: 'privateinternetaccess', score: 5.4,
+                    name: 'Private Internet Access', path: 'privateinternetaccess', score: 5.4,
                     offer: 'https://www.privateinternetaccess.com'
                 },
                 {
-                    name: 'InvisibleBrowsingVPN', path: 'invisiblebrowsingvpn', score: 5.3,
+                    name: 'Invisible Browsing VPN', path: 'invisiblebrowsingvpn', score: 5.3,
                     offer: 'https://www.ibvpn.com'
                 },
                 { name: 'GhostPath', path: 'ghostpath', score: 5.1, offer: 'https://www.ghostpath.com' },
-                { name: 'VPNOneClick', path: 'vpnoneclick', score: 5.0, offer: 'https://www.vpnoneclick.com' },
+                { name: 'VPN One Click', path: 'vpnoneclick', score: 5.0, offer: 'https://www.vpnoneclick.com' },
                 { name: 'WiTopia', path: 'witopia', score: 5.0, offer: 'https://www.witopia.com' },
                 { name: 'Barracuda', path: 'barracuda', score: 5.0, offer: 'https://www.barracuda.com' },
                 { name: 'ProXPN', path: 'proxpn', score: 5.0, offer: 'https://www.secure.proxpn.com' },
@@ -205,13 +200,13 @@ export const vpnReviews: Page = {
                 { name: 'TorGuard', path: 'torguard', score: 5.0, offer: 'https://www.torguard.net' },
                 { name: 'IronSocket', path: 'ironsocket', score: 5.0, offer: 'https://www.hmn.ironsocket.com' },
                 { name: 'VPNJack', path: 'vpnjack', score: 5.0, offer: 'https://www.vpnjack.com' },
-                { name: 'VPNTunnel', path: 'vpntunnel', score: 5.0, offer: 'https://www.vpntunnel.com' },
-                { name: 'Juniper', path: 'juniper', score: 5.0, offer: 'https://www.juniper.net' },
-                { name: 'Unblock-US', path: 'unblockus', score: 5.0, offer: 'https://www.unblock-us.com' },
-                { name: 'GoTrusted', path: 'gotrusted', score: 5.0, offer: 'https://www.gotrusted.com' },
+                { name: 'VPN Tunnel', path: 'vpntunnel', score: 5.0, offer: 'https://www.vpntunnel.com' },
+                { name: 'Juniper Networks', path: 'juniper', score: 5.0, offer: 'https://www.juniper.net' },
+                { name: 'Unblock US', path: 'unblockus', score: 5.0, offer: 'https://www.unblock-us.com' },
+                { name: 'Go Trusted', path: 'gotrusted', score: 5.0, offer: 'https://www.gotrusted.com' },
                 { name: 'AirVPN', path: 'airvpn', score: 5.0, offer: 'https://www.airvpn.org' },
-                { name: 'MyPrivateNetwork', path: 'myprivatenetwork', score: 5.0, offer: 'https://www.my-private-network.co.uk' },
-                { name: 'BolehVPN', path: 'bolehvpn', score: 5.0, offer: 'https://www.bolehvpn.net' },
+                { name: 'My Private Network', path: 'myprivatenetwork', score: 5.0, offer: 'https://www.my-private-network.co.uk' },
+                { name: 'Boleh VPN', path: 'bolehvpn', score: 5.0, offer: 'https://www.bolehvpn.net' },
                 { name: 'LimeVPN', path: 'limevpn', score: 5.0, offer: 'https://www.limevpn.com' },
                 { name: 'SaferWeb', path: 'saferweb', score: 5.0, offer: 'https://www.saferweb.com' },
                 { name: 'SecureVPN', path: 'securevpn', score: 5.0, offer: 'https://www.securevpn.com' },
